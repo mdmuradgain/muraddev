9d426def35ee928a52edea4ad540eb48ba1b44ee
Fresh Dokan <web.programminghero.com.web91.863@gmail.com>
2024-01-21 19:08:46 +0600

    about section added

--- js/script.js
+++ js/script.js
@@ -5,4 +5,9 @@ var typed = new Typed('.typing', {
     typeSpeed: 80,
     backSpeed: 80,
   });
-  
+
+  var typed = new Typed('.typing2', {
+    strings: ["Web Designer", "Web Developer"],
+    typeSpeed: 80,
+    backSpeed: 80,
+  }); 
